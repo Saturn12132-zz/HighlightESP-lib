16c92bda95e646343a69ff4741a3be6cde051966
Saturn12132 <99106795+Saturn12132@users.noreply.github.com>
2022-07-10 16:20:54 -0700

    Update Player Example.lua

--- Player Example.lua
+++ Player Example.lua
@@ -1,7 +1,13 @@
+getgenv().Enabled = true
+if getgenv().Enabled == false then
+getgenv().Disable:Disconnect()
+game.CoreGui:FindFirstChild("HighlightName"):Destroy()
+end
+if getgenv().Enabled == true then
 local espSettings = {
     FillColor = Color3.fromRGB(200, 90, 255),
     OutlineColor = Color3.fromRGB(255, 119, 215),
-    FillTransparency = 0.65,
+    FillTransparency = 0,
     OutlineTransparency = 0,
     DepthMode = Enum.HighlightDepthMode.AlwaysOnTop,
 }
@@ -18,8 +24,9 @@ for i,v in ipairs(Players:GetPlayers()) do
     if v.Character then
         addCharToEsp(v.Character)
     end
-    v.CharacterAdded:Connect(addCharToEsp)
+    getgenv().Disable = v.CharacterAdded:Connect(addCharToEsp)
 end
-Players.PlayerAdded:Connect(function(Player)
-    v.CharacterAdded:Connect(addCharToEsp)
+getgenv().Disable = Players.PlayerAdded:Connect(function(Player)
+    getgenv().Disable = v.CharacterAdded:Connect(addCharToEsp)
 end)
+end
