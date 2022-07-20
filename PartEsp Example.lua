34b3129db610507b8f0f939d666cfb552e1f9747
Saturn12132 <99106795+Saturn12132@users.noreply.github.com>
2022-07-20 12:55:54 -0700

    Update PartEsp Example.lua

--- PartEsp Example.lua
+++ PartEsp Example.lua
@@ -7,7 +7,7 @@ if getgenv().Enabled == true then
 local espSettings = {
     FillColor = Color3.fromRGB(200, 90, 255),
     OutlineColor = Color3.fromRGB(255, 119, 215),
-    FillTransparency = 0.65,
+    FillTransparency = 0,
     OutlineTransparency = 0,
     DepthMode = Enum.HighlightDepthMode.AlwaysOnTop,
 }
@@ -21,9 +21,12 @@ local function addPartToEsp(PartModel)
 end
 
 for i,v in ipairs(Part:GetChildren()) do
-    if v:IsA("Part") then
+    if v:IsA("Model") then
         addPartToEsp(v)
     end
     getgenv().Disable = v.ChildAdded:Connect(addPartToEsp)
 end
+getgenv().Disable = Part.ChildAdded:Connect(function(v)
+getgenv().Disable = v.ChildAdded:Connect(addCharToEsp)
+end)
 end
